8598e32624b12b796027a0e7df9796da2fbd9991
dragon-liu <liuzhichao@hust.edu.cn>
2021-10-01 15:21:48 +0800

    脚本参数

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dragon-liu/todoList-final-version.git master:gh-pages
+git push -f git@github.com:dragon-liu/vue2-happytest.git master:gh-pages
 
 cd -
